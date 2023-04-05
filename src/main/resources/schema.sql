55937531a18249d6c665669aedc78fdc02a602fb
questionant777 <119503924+questionant777@users.noreply.github.com>
2023-04-05 11:21:20 +0300

    Add files via upload

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+drop table if exists author;
+create table author(id bigint primary key auto_increment, name varchar(255));
+
+drop table if exists genre;
+create table genre(id bigint primary key auto_increment, name varchar(255));
+
+drop table if exists book;
+create table book(id bigint primary key auto_increment, name varchar(255), authorid bigint, genreid bigint);
+
+drop table if exists bookcomment;
+create table bookcomment(id bigint primary key auto_increment, comment varchar(255), bookid bigint);
+
+drop table if exists appuser;
+create table appuser(
+    id bigint primary key auto_increment,
+    name varchar(255),
+    password varchar(255),
+    role varchar(32)
+);
---
+++ src/test/java/ru/otus/spring/service/AuthorServiceImplTest.java
@@ -0,0 +1,55 @@
+package ru.otus.spring.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.spring.repository.AuthorRepository;
+import ru.otus.spring.domain.Author;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthorServiceImplTest {
+
+    public static final long NEW_AUTHOR_ID = 1L;
+    public static final String NEW_AUTHOR_NAME = "new author name";
+    public static final long EXISTING_AUTHOR_ID = 5L;
+    public static final String EXISTING_AUTHOR_NAME = "existing author name";
+
+    @Mock
+    AuthorRepository authorDao;
+
+    @InjectMocks
+    AuthorServiceImpl service;
+
+    @Test
+    void getOrSaveByNameForExistingAuthorTest() {
+        Author expectedAuthor = new Author(EXISTING_AUTHOR_ID, EXISTING_AUTHOR_NAME);
+
+        when(authorDao.findByName(EXISTING_AUTHOR_NAME))
+                .thenReturn(Optional.of(expectedAuthor));
+
+        Author actualAuthor = service.getOrSaveByName(EXISTING_AUTHOR_NAME);
+
+        assertThat(actualAuthor).usingRecursiveComparison().isEqualTo(expectedAuthor);
+    }
+
+    @Test
+    void getOrSaveByNameSaveAndRetNotExistingAuthorTest() {
+        Author newAuthor = new Author(NEW_AUTHOR_ID, NEW_AUTHOR_NAME);
+
+        when(authorDao.save(any()))
+                .thenReturn(newAuthor);
+
+        Author actualAuthor = service.getOrSaveByName(NEW_AUTHOR_NAME);
+
+        assertThat(actualAuthor).usingRecursiveComparison().isEqualTo(newAuthor);
+    }
+
+}
